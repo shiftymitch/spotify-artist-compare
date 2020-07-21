2bad8ce0f337d4d06e8e482e740da51284921f0c
Mitch Henderson <shiftymitch@gmail.com>
2020-07-21 11:30:04 -0600

    fixing build

---
+++ build/precache-manifest.35e52c32041c7acc8743b96c7f186907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa9bf53c4344897ff368f3c735475ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "453c0047df17089182de",
+    "url": "/static/css/main.c0cb8c11.chunk.css"
+  },
+  {
+    "revision": "b63db1f06bb06448b1b9",
+    "url": "/static/js/2.edbcb868.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.edbcb868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453c0047df17089182de",
+    "url": "/static/js/main.44890695.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);

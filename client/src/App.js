37bead9c2eeab0f284284d0ded55a22b6874abf1
Mitch Henderson <shiftymitch@gmail.com>
2020-07-21 14:08:46 -0600

    removed default single search page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav/Nav.jsx"
 import Footer from "./components/Footer.jsx"
-import Home from './components/Home.jsx';
 import Compare from './components/Compare.jsx';
 import "./App.css";
 
@@ -11,8 +10,7 @@ function App() {
     <Router>
       <Nav />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/compare" component={Compare} />
+          <Route exact path="/" component={Compare} />
         </Switch>
       <Footer />
     </Router>
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 function Nav() {
   return (
       <div className="navbar navbar-expand-lg position-sticky justify-content-between">
         <p className="navbar-brand ml-5 mr-5"><img src="/spotify_icon.png" alt="spotify-icon"></img><strong className="ml-3">Spotify Artist Compare</strong></p>
-        <Link to={() => {
-          if (window.location.pathname === "/compare") {
-            return `/`;
-          } else {
-            return "/compare";
-          }
-        }}><button className="navbar-item btn btn-sm btn-outline-success mr-auto" id="compare" >Compare</button></Link>
       </div>
   );
 }

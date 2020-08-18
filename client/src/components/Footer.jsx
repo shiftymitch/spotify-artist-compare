23177c7495907ea41c03bbe6d3eb28cf9816154c
Mitch Henderson <shiftymitch@gmail.com>
2020-08-18 17:08:25 -0600

    styling updates

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import moment from "moment";
 function Footer() {
   return (
     <div className="footer">
-      <p className="row">© Copyright {moment().format("YYYY")} | Built by:<a className="ml-1" href="https://github.com/shiftymitch" target="_blank" rel="noopener noreferrer">Mitch Henderson</a></p>
+      <p className="row">Built by<a className="ml-1" href="https://github.com/shiftymitch" target="_blank" rel="noopener noreferrer">Mitch Henderson</a></p>
     </div>
   );
 }

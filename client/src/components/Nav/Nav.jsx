ceeb00c27b296e4520b0cf3fe87ae843facf34dd
Mitch Henderson <shiftymitch@gmail.com>
2020-08-18 11:22:56 -0600

    removed compare button

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -8,13 +8,6 @@ function Nav() {
           <img src="/spotify_icon.svg" id="spotify-icon" alt="spotify-icon"></img>
           <p id="navbar-name"><strong>Artist Compare</strong></p>
         </div>
-        <Link to={() => {
-          if (window.location.pathname === "/compare") {
-            return `/`;
-          } else {
-            return "/compare";
-          }
-        }}><button className="navbar-item btn btn-sm btn-outline-success mr-auto" id="compare" >Compare</button></Link>
       </div>
   );
 }

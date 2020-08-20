ee4bf962dfb0cb2c606a08eac5fd4d72aeccd0dc
Mitch Henderson <shiftymitch@gmail.com>
2020-08-19 21:09:41 -0600

    styling updates

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -3,31 +3,23 @@ import "./style.css";
 
 function SearchForm(props) {
   return (
-    <div className="search">
-      <div className="col-2"></div>
-      <form className="col-6">
-        <div className="form-group mb-3">
-          <input
-            value={props.search}
-            onChange={props.handleInputChange}
-            name="search"
-            list="results"
-            type="text"
-            className="form-control"
-            placeholder="Search for an artist..."
-            id={"search-" + props.count}
-          />
-          <button id={"search-btn-" + props.count} type="submit" onClick={props.handleFormSubmit} className="btn btn-outline-success form-control">
-            Search
-          </button>
-        </div>
-      </form>
-      <div className="col-2 login-helper">
-        <p id="lightbulb"><i className="fa fa-lightbulb-o" ></i></p>
-        <p>Log in to Spotify.com to listen to full tracks.</p>
-        
+    <form className="search text-center">
+      <div className="form-group">
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="search"
+          list="results"
+          type="text"
+          className="form-control"
+          placeholder="Search for an artist..."
+          id={"search-" + props.count}
+        />
+        <button id={"search-btn-" + props.count} type="submit" onClick={props.handleFormSubmit} className="btn btn-outline-success form-control">
+          Search
+        </button>
       </div>
-    </div>
+    </form>
   );
 }
 
